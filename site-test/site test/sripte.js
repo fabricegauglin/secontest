98f4ee9a40c52a9c1386ceef56d1868662008288
fabricegauglin <gauglin.fabrice.at@gmail.com>
2019-01-20 17:03:37 +0100

    ajout du 2 test

---
+++ site-test/site test/sripte.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    
+    $("li").hide();
+    $('h1').hide();
+    $("h2").hide();
+    $(".par1").hide();
+    $("#titel1").hide();
+    $("p:nth-child(2)").hide();
+    
+    $("h6").click(function(){
+        $("h6").hide();
+        });
+    
+    $("#h3").mouseenter(function(){
+        $("h5").hide();
+        });
+    
+    $("#h4").mouseleave(function(){
+        $("#h7").show();
+    $("h8").hover(function(){
+        $("h9").hide();
+        },
+                  
+    function(){
+        $("h9").show();
+        
+    $("figcaption").click(function(){
+        $("img").hide();
+         });    
+        
+       }); 
+        
+    });
+    
+});
