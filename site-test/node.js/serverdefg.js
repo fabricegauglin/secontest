230321df44aebad27946c5ecf2faed0a4da53bfe
fabricegauglin <gauglin.fabrice.at@gmail.com>
2019-01-25 14:56:27 +0100

    a voire les module qui plante ;)»

--- site-test/node.js/serverdefg.js
+++ site-test/node.js/serverdefg.js
@@ -1,1 +1,17 @@
-console.log('salut les petits lu dans le café')
+console.log('salut les petits lu dans le café et également il a u une requet de rocket')
+
+let http = require('http')
+
+
+let server = http.createServer()
+server.on('request', (request, response) => {
+  
+  response.writeHead(200)
+  response.end('salut coment ça va pas ?')
+  
+  
+  
+  
+})
+
+server.listen(8080)
